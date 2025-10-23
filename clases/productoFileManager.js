f4d26cdf0dcf75d02727e9aa1159b481dfd069e8
7N M-J <carsasgar@alu.edu.gva.es>
2025-10-23 19:54:36 +0200

    Último intento de hoy (mal)

--- clases/productoFileManager.js
+++ clases/productoFileManager.js
@@ -7,9 +7,9 @@ import { Producto } from './producto.js';
 export class productoFileManager {
     leerArchivo() {
         console.log(path);
-        fs.access(path,
+        fs.access(path, fs.constants.F_OK,
             new Promise((resolve, reject) => {
-                fs.readFile(path,
+                fs.readFile(path, fs.constants.F_OK,
                     new Promise((resolve, reject) => {
                         resolve("resolve en readFile");
                         reject("reject en readFile");
@@ -28,7 +28,7 @@ export class productoFileManager {
     }
 
     guardarArchivo(data) {
-        fs.access(path,
+        fs.access(path, fs.constants.F_OK,
             new Promise((resolve, reject) => {
                 fs.writeFile(path, JSON.stringify(data),
                     new Promise((resolve, reject) => {

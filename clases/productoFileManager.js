8dce5db9c4dc242be591859d900316888690dcd4
7N M-J <carsasgar@alu.edu.gva.es>
2025-10-23 19:47:30 +0200

    Probar cambios (mal)

--- clases/productoFileManager.js
+++ clases/productoFileManager.js
@@ -5,8 +5,8 @@ const path = "../productos.json";
 import { Producto } from './producto.js';
 
 export class productoFileManager {
-
     leerArchivo() {
+        console.log(path);
         fs.access(path,
             new Promise((resolve, reject) => {
                 fs.readFile(path,
@@ -27,14 +27,23 @@ export class productoFileManager {
         )
     }
 
-    guardarArchivo() {
+    guardarArchivo(data) {
         fs.access(path,
             new Promise((resolve, reject) => {
-                fs.writeFile(path,
+                fs.writeFile(path, JSON.stringify(data),
                     new Promise((resolve, reject) => {
-                        console.log(hola);
+                        resolve("resolve en writeFile");
+                        reject("reject en writeFile");
                     })
-                )
+                );
+                resolve("resolve en access");
+                reject("reject en access");
+            })
+            .then((resolve) => {
+                console.log(resolve);
+            })
+            .catch((error) => {
+                console.log(error);
             })
         )
     }

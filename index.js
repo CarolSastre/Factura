828b1e7638a91b5cd9d4eaf252614a28420af2e8
7N 192.168.207.15 <carsasgar@alu.edu.gva.es>
2025-11-26 20:46:28 +0100

    Ventanita

--- index.js
+++ index.js
@@ -12,244 +12,10 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+let altaWindow;
 
-/////
-// Simple Menu Sample
-/////
-let template1 = [{
-  label: 'Menu 1',
-  submenu: [{
-    label: 'Menu item 1'
-  }]
-}, {
-  label: 'Menu 2',
-  submenu: [{
-    label: 'Another Menu item'
-  }, {
-    label: 'One More Menu Item'
-  }, {
-    label: 'Generate Icon',
-    click: doGenerateIcon
-  }
-  ]
-}]
-
-///////
-// Basic Edit and Window Menu sample
-///////
-let template2 = [{
-  label: 'Edit App',
-  submenu: [{
-    label: 'Undo',
-    accelerator: 'CmdOrCtrl+Z',
-    role: 'undo'
-  }, {
-    label: 'Redo',
-    accelerator: 'Shift+CmdOrCtrl+Z',
-    role: 'redo'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Cut',
-    accelerator: 'CmdOrCtrl+X',
-    role: 'cut'
-  }, {
-    label: 'Copy',
-    accelerator: 'CmdOrCtrl+C',
-    role: 'copy'
-  }, {
-    label: 'Paste',
-    accelerator: 'CmdOrCtrl+V',
-    role: 'paste'
-  }, {
-    label: 'Select All',
-    accelerator: 'CmdOrCtrl+A',
-    role: 'selectall'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'My Submenu',
-    submenu: [
-      {
-        label: 'Item 1'
-      },
-      {
-        label: 'Item 2'
-      }
-    ]
-  }]
-}];
-
-let windowMenu = {
-  label: 'Window',
-  role: 'window',
-  submenu: [{
-    label: 'Minimize',
-    accelerator: 'CmdOrCtrl+M',
-    role: 'minimize'
-  }, {
-    label: 'Close',
-    accelerator: 'CmdOrCtrl+W',
-    role: 'close'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Reopen Window',
-    accelerator: 'CmdOrCtrl+Shift+T',
-    enabled: false,
-    key: 'reopenMenuItem',
-    click: function () {
-      app.emit('activate')
-    }
-  }]
-};
-
-template2.push(windowMenu);
-
-////
-//Complete Starter Sample
-/////
-let template3 = [{
-  label: 'Edit',
-  submenu: [{
-    label: 'Undo',
-    accelerator: 'CmdOrCtrl+Z',
-    role: 'undo'
-  }, {
-    label: 'Redo',
-    accelerator: 'Shift+CmdOrCtrl+Z',
-    role: 'redo'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Cut',
-    accelerator: 'CmdOrCtrl+X',
-    role: 'cut'
-  }, {
-    label: 'Copy',
-    accelerator: 'CmdOrCtrl+C',
-    role: 'copy'
-  }, {
-    label: 'Paste',
-    accelerator: 'CmdOrCtrl+V',
-    role: 'paste'
-  }, {
-    label: 'Select All',
-    accelerator: 'CmdOrCtrl+A',
-    role: 'selectall'
-  }]
-}, {
-  label: 'View',
-  submenu: [{
-    label: 'Reload',
-    accelerator: 'CmdOrCtrl+R',
-    click: function (item, focusedWindow) {
-      if (focusedWindow) {
-        // on reload, start fresh and close any old
-        // open secondary windows
-        if (focusedWindow.id === 1) {
-          BrowserWindow.getAllWindows().forEach(function (win) {
-            if (win.id > 1) {
-              win.close()
-            }
-          })
-        }
-        focusedWindow.reload()
-      }
-    }
-  }, {
-    label: 'Toggle Full Screen',
-    accelerator: (function () {
-      if (process.platform === 'darwin') {
-        return 'Ctrl+Command+F'
-      } else {
-        return 'F11'
-      }
-    })(),
-    click: function (item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-      }
-    }
-  }, {
-    label: 'Toggle Developer Tools',
-    accelerator: (function () {
-      if (process.platform === 'darwin') {
-        return 'Alt+Command+I'
-      } else {
-        return 'Ctrl+Shift+I'
-      }
-    })(),
-    click: function (item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.toggleDevTools()
-      }
-    }
-  }, {
-    type: 'separator'
-  }, {
-    label: 'My Submenu',
-    submenu: [
-      {
-        label: 'Item 1',
-        type: 'checkbox',
-        checked: true
-      }, {
-        label: 'Item 2',
-        type: 'checkbox',
-        checked: false
-      }, {
-        label: 'Item 3',
-        type: 'radio',
-        checked: true
-      }, {
-        label: 'Item 4',
-        type: 'radio',
-        checked: false
-      }]
-  }]
-}, {
-  label: 'Window',
-  role: 'window',
-  submenu: [{
-    label: 'Minimize',
-    accelerator: 'CmdOrCtrl+M',
-    role: 'minimize'
-  }, {
-    label: 'Close',
-    accelerator: 'CmdOrCtrl+W',
-    role: 'close'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Reopen Window',
-    accelerator: 'CmdOrCtrl+Shift+T',
-    enabled: false,
-    key: 'reopenMenuItem',
-    click: function () {
-      app.emit('activate')
-    }
-  }]
-}, {
-  label: 'Help',
-  role: 'help',
-  submenu: [{
-    label: 'Learn More',
-    click: function () {
-      electron.shell.openExternal('http://electron.atom.io')
-    }
-  }]
-}]
-
-template3[1].submenu.push({
-  type: 'separator'
-}, {
-  label: 'Bring All to Front',
-  role: 'front'
-})
-
-let template4 = [{
+let menuTemplate = [{
   label: 'Edit App',
   submenu: [{
     label: 'Undo',
@@ -416,12 +182,6 @@ let template4 = [{
   }]
 }]
 
-function doGenerateIcon(menuItem, browserWindow, evt) {
-  console.log(menuItem)
-  console.log(browserWindow)
-  console.log(evt)
-}
-
 /////
 //Contextual Menu
 //////
@@ -442,13 +202,23 @@ ipcMain.on('show-context-menu', function (event) {
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
+    title: "Factura v.3",
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
+  })
 
-    //win.webContents.openDevTools()
+  altaWindow = new BrowserWindow({
+    show: true,
+    width: 350,
+    height: 350,
+    resizable: false,
+    title: "Dar de alta un producto",
+    webPreferences: {
+      preload: path.join(__dirname, 'preloadWindow.js')
+    }
   })
 
   // and load the index.html of the app.
@@ -458,8 +228,18 @@ function createWindow() {
     slashes: true
   }))
 
+  altaWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'altaProducto.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  // Wait for 'ready-to-show' to display our window
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -481,11 +261,18 @@ app.on('ready', function () {
   ipcMain.handle('createProdFile', createProdFile);
   ipcMain.handle('createDir', createDir);
 
-  const menu = Menu.buildFromTemplate(template4);
+  const menu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(menu);
   createWindow();
 })
 
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -493,14 +280,6 @@ app.on('window-all-closed', function () {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
   }
 })
 

2b553523d820609243dafbb3ac04dc2b73b8fbc7
7N 192.168.207.15 <carsasgar@alu.edu.gva.es>
2025-12-03 20:47:33 +0100

    cambiar nombres

---
+++ renderer.js
@@ -0,0 +1,22 @@
+import { Controller } from './src/controller/controller.js';
+
+const controller = new Controller();
+
+window.onload = () => {
+  // addEventListeners de la aplicacion
+
+  document.getElementById("logo").addEventListener('click', () => controller.promptWindow());
+
+  document.getElementById("factura").addEventListener('change', () => controller.cargarFactura());
+
+  document.getElementById("btnBorrarCesta").addEventListener('click', () => controller.borraFactura(true));
+
+  document.getElementById("btnCrearFactura").addEventListener('click', () => controller.guardaFactura(0))
+  document.getElementById("btnModificarFactura").addEventListener('click', () => controller.guardaFactura(1))
+
+  document.getElementById("btnEliminarFactura").addEventListener('click', () => controller.eliminaFactura())
+
+  document.getElementById("btnAnadirCarrito").addEventListener('click', () => controller.anyadirFilaFactura());
+
+  controller.init();
+}
